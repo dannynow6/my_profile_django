50037638be9af0e2a9d34c5b19708ec19498e7bd
Dan Garro <dobetterwithdan@gmail.com>
2023-02-07 10:39:54 -0500

    started defining models / no migrations performed

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,62 @@
 from django.db import models
 
-# Create your models here.
+# Porfolio App Models
+
+
+class Project(models.Model):
+    """A model representing projects within my portfolio"""
+
+    git_repo = models.URLField(max_length=300, blank=True)
+    title = models.CharField(max_length=250, unique=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    created_with = models.CharField(max_length=400)
+    description = models.CharField(max_length=400)
+
+    def __str__(self) -> str:
+        """return a string representation of Project model"""
+        title = self.title.title()
+        repo = self.git_repo
+        return f"{title} | {repo}"
+
+
+class WorkExperience(models.Model):
+    """A model representing my work experience from resume"""
+
+    position_title = models.CharField(max_length=225)
+    company_name = models.CharField(max_length=300)
+    company_location = models.CharField(max_length=250)
+    dates_employed = models.CharField(max_length=250)
+    job_description = models.CharField(max_length=350)
+    additional_job_description = models.CharField(max_length=400, blank=True)
+
+    def __str__(self) -> str:
+        """return a string representation of WorkExperience"""
+        position = self.position_title.title()
+        company = self.company_name.title()
+        dates = self.dates_employed
+        return f"{position} | {company} | {dates}"
+
+
+class OtherWorkExperience(models.Model):
+    """A model representing other work experience from resume"""
+
+    position_title = models.CharField(max_length=225)
+    company_name = models.CharField(max_length=300)
+    company_location = models.CharField(max_length=250)
+    dates_employed = models.CharField(max_length=250)
+    job_description = models.CharField(max_length=350)
+    additional_job_description = models.CharField(max_length=400, blank=True)
+    specialization = models.CharField(max_length=250, blank=True)
+    special_info = models.CharField(max_length=250, blank=True)
+    special_info_2 = models.CharField(max_length=250, blank=True)
+
+    def __str__(self) -> str:
+        """return a string representation of WorkExperience"""
+        position = self.position_title.title()
+        company = self.company_name.title()
+        dates = self.dates_employed
+        return f"{position} | {company} | {dates}"
+
+
+class Education(models.Model):
+    """A model representing education history"""

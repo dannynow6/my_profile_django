2b4367a8c99acd136f44c980a91641fcb7bdcbcc
Dan Garro <dobetterwithdan@gmail.com>
2023-02-07 19:11:34 -0500

    queryed my links and added to page

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+from .models import MyLinks 
 
 def index(request):
     """the home page for my portfolio app"""
-    return render(request, "portfolio/index.html")
+    links = MyLinks.objects.all() 
+    context = {"links": links}
+    return render(request, "portfolio/index.html", context)

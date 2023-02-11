6ac5b66a1f848b5941bf94dd74cb1fdcbfaac4d2
Dan Garro <dobetterwithdan@gmail.com>
2023-02-11 18:56:56 -0500

    updated image for logo

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from .models import MyLinks 
+from .models import Project 
+
 
 def index(request):
     """the home page for my portfolio app"""
     links = MyLinks.objects.all() 
     context = {"links": links}
     return render(request, "portfolio/index.html", context)
+
+

4ef8b3351b36363056b567cefa0fa7f0232fc364
Dan Garro <dobetterwithdan@gmail.com>
2023-02-12 13:47:26 -0500

    started making basic pages for history app of portfolio

---
+++ history/urls.py
@@ -0,0 +1,11 @@
+"""Defines URL patterns for History App - portfolio"""
+from django.urls import path
+from . import views
+
+app_name = "history"
+urlpatterns = [
+    # A page for viewing education history details
+    path("my_education/", views.my_education, name="my_education"),
+    # A page for viewing work experience 
+    path("work_experience/", views.work_experience, name="work_experience"), 
+]
--- history/views.py
+++ history/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .models import WorkExperience
+from .models import OtherWorkExperience
+from .models import Education
+from .models import Skill
+from .models import SoftSkill
+from .models import Hobby
 
-# Create your views here.
+
+# Views for History App of portfolio
+
+def my_education(request):
+    """A view for displaying Education information"""
+    edu = Education.objects.all()
+    context = {"edu": edu}
+    return render(request, "history/my_education.html", context)
+
+def work_experience(request):
+    """a page for viewing work experience"""
+    work = WorkExperience.objects.all() 
+    otherwork = OtherWorkExperience.objects.all() 
+    context = {'work': work, 'otherwork': otherwork} 
+    return render(request, "history/work_experience.html", context) 

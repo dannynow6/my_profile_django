e1b884492b33e193a90f53845ebdebd10d848345
Dan Garro <dobetterwithdan@gmail.com>
2023-02-11 21:36:50 -0500

    added a page for viewing projects, viewing individual projects, and included pdf creation

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -6,4 +6,10 @@ app_name = "portfolio"
 urlpatterns = [
     # Home page 
     path("", views.index, name="index"), 
+    # A page for viewing featured projects 
+    path("projects/", views.projects, name="projects"), 
+    # A path to generate a pdf of project 
+    path("<int:project_id>/print/", views.print_project, name="print_project"), 
+    # A page to view a single project 
+    path("<int:project_id>/", views.project, name="project"), 
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,12 +1,79 @@
 from django.shortcuts import render
-from .models import MyLinks 
-from .models import Project 
+from .models import MyLinks
+from .models import Project
+import datetime
+
+# Imports for using ReportLab for PDF gen
+import io
+from django.http import FileResponse
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter
+from reportlab.lib.styles import getSampleStyleSheet
 
 
 def index(request):
     """the home page for my portfolio app"""
-    links = MyLinks.objects.all() 
+    links = MyLinks.objects.all()
     context = {"links": links}
     return render(request, "portfolio/index.html", context)
 
 
+def projects(request):
+    """A page for viewing current featured projects"""
+    projects = Project.objects.all()
+    context = {"projects": projects}
+    return render(request, "portfolio/projects.html", context)
+
+
+def print_project(request, project_id):
+    # Need to add a view for viewing projects - view all in list with view/print options
+    buffer = io.BytesIO()
+    p = canvas.Canvas(buffer, pagesize=letter)
+    project = Project.objects.get(id=project_id)
+    repo = project.git_repo
+    title = project.title
+    date = project.date_added  # use .strftime('%m/%d/%Y')
+    created_with = project.created_with
+    description = project.description
+    project_id = project.id
+
+    logo = "./static/img/dg.jpg"
+    p.drawImage(logo, 216, 576, width=None, height=None)
+
+    title_obj = p.beginText(270, 504)
+    title_obj.setFont("Helvetica-Oblique", 14, leading=None)
+    title_obj.textLine(text=f"{title.title()}")
+    p.drawText(title_obj)
+
+    date_obj = p.beginText(540, 468)
+    date_obj.setFont("Helvetica", 11, leading=None)
+    date_obj.textLine(text=f"{date.strftime('%m/%d/%Y')}")
+    p.drawText(date_obj)
+
+    create_obj = p.beginText(72, 400)
+    create_obj.setFont("Helvetica", 11, leading=None)
+    create_obj.textLine(text=f"Created using: {created_with}")
+    p.drawText(create_obj)
+
+    des_obj = p.beginText(72, 328)
+    des_obj.setFont("Helvetica", 11, leading=None)
+    des_obj.textLine(text=f"{description}")
+    p.drawText(des_obj)
+
+    repo_obj = p.beginText(72, 256)
+    repo_obj.setFont("Helvetica", 11, leading=None)
+    repo_obj.textLine(text=f"{repo}")
+    p.drawText(repo_obj)
+
+    p.showPage()
+    p.save()
+
+    buffer.seek(0)
+    return FileResponse(buffer, as_attachment=True, filename=f"project{project_id}.pdf")
+
+
+def project(request, project_id):
+    """a page for viewing the details of a single project"""
+    project = Project.objects.get(id=project_id)
+    context = {"project": project}
+    return render(request, "portfolio/project.html", context)
